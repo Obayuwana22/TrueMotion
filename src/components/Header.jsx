5c12341b015bb84c2aafe68730de7e6ef8e7c68a
Obayuwana22 <successobayuwana@gmail.com>
2025-04-29 06:23:54 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Search from "./Search";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <h1>truemotion</h1>
+        <Search />
+        
+      </div>
+    </div>
+  );
+};
+export default Header;
