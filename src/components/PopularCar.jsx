8974e87c7c9ec62edddf0dab76fc00bcd92cd5b4
Obayuwana22 <successobayuwana@gmail.com>
2025-05-04 19:41:42 +0300

    Add blur effect to popular cars section

--- src/components/PopularCar.jsx
+++ src/components/PopularCar.jsx
@@ -7,11 +7,14 @@ const PopularCar = () => {
         <div className="text-sm text-secondary-300">Popular Car</div>
         <div className="text-xs text-primary-500">View All</div>
       </div>
-      <div className="flex gap-5 overflow-auto scrollbar-hide">
-        <PopularCarCard />
-        <PopularCarCard />
-        <PopularCarCard />
-        <PopularCarCard />
+      <div className="relative">
+        <div className="flex gap-5 overflow-auto scrollbar-hide">
+          <PopularCarCard />
+          <PopularCarCard />
+          <PopularCarCard />
+          <PopularCarCard />
+        </div>
+        <div className="absolute right-0 top-0 h-full w-16 z-10 bg-gradient-to-l from-white/80 to-transparent pointer-events-none" />
       </div>
     </div>
   );
