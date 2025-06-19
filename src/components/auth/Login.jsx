45857edf9a58fa3bceaa7086da7e1e3984d4dd92
success obayuwana <successobayuwana@gmail.com>
2025-06-19 15:29:30 +0100

    Fix login layout bug

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -43,8 +43,9 @@ const Login = () => {
                 <Lock className="text-primary-white" />
               </div>
               <h1 className="font-semibold">Welcome Back</h1>
-              <p className="mb-5 text-secondary-400 text-center ">
+              <p className="mb-5 text-secondary-400 text-center mx-5 ">
                 Sign in to your account to continue
+                
               </p>
             </div>
 
