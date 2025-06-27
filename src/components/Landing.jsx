5a1694e14c4326404c3d325037dbe979cae0874b
success obayuwana <successobayuwana@gmail.com>
2025-06-27 15:48:57 +0100

    Add banner call to action section

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,13 +1,13 @@
 import Hero from "./Hero";
 import FeaturedSection from "./FeaturedSection";
-
+import Banner from "./Banner";
 
 const Landing = () => {
   return (
     <div>
       <Hero />
       <FeaturedSection />
-
+      <Banner />
     </div>
   );
 };
