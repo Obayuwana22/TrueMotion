8da99e861bbe58e83c90dcd41cc9520bbccdfc2f
success obayuwana <successobayuwana@gmail.com>
2025-06-29 17:57:09 +0100

    Fix some bugs

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
 import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
 import Footer from "./Footer";
 
 const HomeLayout = () => {
-  const [showLogin, setShowLogin] = useState(false);
+  // const [showLogin, setShowLogin] = useState(false);
   const isOwnerPath = useLocation().pathname.startsWith("/owner");
   return (
     <div>
-      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      {!isOwnerPath && <Navbar />}
       <Outlet />
       {!isOwnerPath && <Footer />}
     </div>
